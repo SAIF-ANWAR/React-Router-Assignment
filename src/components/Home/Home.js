050f4c9ccd004c9cb71959de8c2855c368f0db6b
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-04 14:47:55 +0600

    reviews

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,13 +12,15 @@ const Home = ({ logo }) => {
     return (
         <div>
             <div className='flex justify-between'>
-                <div className='p-7 pt-10 mt-10 px-5'>
-                    <h1 className='text-5xl font-bold'>Tour De Bangladesh</h1>
-                    <h2 className='text-4xl font-bold mt-2'>Find the best places to visit in Bangladesh</h2>
-                    <p className='text-xl mt-3'>Travel Bangladesh is a website that promotes travel in Bangladesh. You can find the information
-                        about which  places to visit in  this country. You will also find the best places to travel and
-                        the most  reviewed places in Bangladesh.
-                    </p>
+                <div className='p-5 pt-10 mt-10'>
+                    <div className='px-8'>
+                        <h1 className='text-5xl font-bold'>Tour De Bangladesh</h1>
+                        <h2 className='text-4xl font-bold mt-2'>Find the best places to visit in Bangladesh</h2>
+                        <p className='text-xl mt-3'>Travel Bangladesh is a website that promotes travel in Bangladesh. You can find the information
+                            about which  places to visit in  this country. You will also find the best places to travel and
+                            the most  reviewed places in Bangladesh.
+                        </p>
+                    </div>
                 </div>
 
                 <div className='w-4/5 pt-10 mx-3'>
