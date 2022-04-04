f62380b4d3556341f83cb59b3cf89b2493382ee8
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-04 15:41:46 +0600

    blog-page-done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs Page</h1>
+        <div className='p-10 '>
+            <div className='border shadow-lg text-xl  p-4'>
+                <h1 className='text-2xl font-bold pb-2'>What is Context API?</h1>
+                <p>Ans: Context API is a React feature that allows  to share state across the entire application. It actually creates a global variable that can be passes to any components. Context API returns a consumer and a provider. When we need to share a state in multiple components, we have to use context API. We can pass the data through props.</p>
+
+            </div>
+            <div className='border shadow-lg text-xl  p-4'>
+                <h1 className='text-2xl font-bold pb-2'>Semantic tags?</h1>
+                <p>
+                    Ans:   Semantic tags are HTML tags that provides meaning to the webpage. Semantic tags are the medium of communication between a webpage and browser. It helps browser to understand what the webpage is all about. For example: h1 tag is a semantic tag because it means a header. Other examples are blockquote, p, span, strong tags etc.
+                </p>
+            </div>
         </div>
     );
 };
